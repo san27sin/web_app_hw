b3d7acc350c588f627019e320bba88d0ccf6ca63
Alex <sin27san@gmail.com>
2022-11-11 09:05:27 +0300

    Необходимо реализовать ASP.NET Core MVC приложение с разделением логики на уровень контроллеров, сервисов и репозиториев. Для уровня сервисов и репозиториев нужно использовать интерфейсы. Приветствуется соблюдение принципов SOLID а также применение Dependency Injection встроенными средствами платформы ASP.NET Core

--- web_app_hw/Models/Requests/CreateClientRequest.cs
+++ web_app_hw/Models/Requests/CreateClientRequest.cs
@@ -1,14 +1,18 @@
 ﻿
 
+using FitnessClub.Data;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
 namespace web_app_hw.Models.Requests
 {
     public class CreateClientRequest
-    {
+    {        
+        public int Id { get; set; } //автоматическое создание
         public string Name { get; set; }
         public string Surname { get; set; }
         public DateTime BirthDay { get; set; }
         public string Membership { get; set; }
-
-        public string club { get; set; }
     }
 }
